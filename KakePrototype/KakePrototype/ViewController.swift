fb1b4dec1ec7f1a60a3a75e430341c3648068a19
Yoshinori Shibahara <yoshinori.shibahara@gmail.com>
2021-02-08 13:26:17 +0900

    First commit

---
+++ KakePrototype/KakePrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KakePrototype
+//
+//  Created by Yoshinori Shibahara on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
